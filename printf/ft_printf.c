3a036b220cf4106df44c3ff78732264d72108ba4
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-10 12:53:03 -0300

    include the header file at the printf function

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,11 +6,29 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 17:35:42 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/08/06 17:37:58 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/08/10 12:34:57 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libftprintf.h"
+
 int	ft_printf(const char *format, ...)
 {
+	va_list	args;
+
+	va_start(args, format);
+	if (!format)
+		return (0);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			/* verify the next char
+			make a function to verify wich function may be called based on the next char
+			after execute the function, move the pointer forward
+			*/
+		}
+		
+	}
 	
 }
