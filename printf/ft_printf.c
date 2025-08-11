cda8b2a686458b6fcc528b681b4fbe88e9a5b3fe
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-11 12:35:23 -0300

    add the libft

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 17:35:42 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/08/10 12:34:57 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/08/11 12:28:40 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,28 @@ int	ft_printf(const char *format, ...)
 		
 	}
 	
+}
+
+static	int	validate_format_specifier(char *str)
+{
+	if (str[1] == 'c')
+		return (1);
+	else if (str[1] == 's')
+		return (2);
+	else if (str[1] == 'p')
+		return (3);
+	else if (str[1] == 'd')
+		return (4);
+	else if (str[1] == 'i')
+		return (5);
+	else if (str[1] == 'u')
+		return (6);
+	else if (str[1] == 'x')
+		return (7);
+	else if (str[1] == 'X')
+		return (8);
+	else if (str[1] == '%')
+		return (9);
+	else
+		return (0);
 }
