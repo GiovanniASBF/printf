70468f741de033a2559be6502bbf509193c39890
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-11 14:21:21 -0300

    add ft_puthex_fd

--- printf/ft_puthex_fd.c
+++ printf/ft_puthex_fd.c
@@ -1,21 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_puthex_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/10 11:50:52 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/08/11 14:19:26 by gaguiar-         ###   ########.fr       */
+/*   Created: 2025/08/11 13:33:35 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/08/11 14:18:37 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#include	"libftprintf.h"
 
-# include <stdarg.h>
-# include "libft.h"
+void	ft_puthex_fd(int n, int fd)
+{
+	int	remainder;
 
-void	ft_puthex_fd(int n, int fd);
-
-#endif
+	if (n == 0)
+	{
+		write(fd, "0", 1);
+		return ;
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_puthex_fd(-n, fd);
+	}
+	if ((n / 16) != 0)
+		ft_puthex_fd((n / 16), fd);
+	remainder = n % 16;
+	if (remainder < 10)
+		ft_putchar_fd(remainder + '0', fd);
+	else
+		ft_putchar_fd(remainder - 10 + 'A', fd);
+}
