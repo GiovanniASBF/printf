c96fd4b6fff4187bc76ef85cce6e823fce00c2dc
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-15 01:59:10 -0300

    add printer
    This class is the main output's effectively responsible for.

---
+++ printf/printer.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printer.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 01:58:48 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/08/15 01:58:49 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	print_char(char c, int *count)
+{
+	write(1, &c, 1);
+	count++;
+}
+
+void	print_str(char *str, int *count)
+{
+	while (*str++)
+		print_char(*str, count);
+}
