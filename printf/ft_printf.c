fb25b160ff588851b2a827e9e4a08c9e02483a15
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-15 01:39:53 -0300

    PROJECT REFACTOR
    I decided to refactor the entire project, starting with ft_printf.c
    The idea is to increase modularization by better separating functions by classes.

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,80 +6,22 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 17:35:42 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/08/11 16:12:05 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/08/15 01:45:49 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
-
-static	void	printvalue(int specifier, va_list args, int *ptrcount);
-static	int	validate_format_specifier(char *str);
+#include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		count;
-	int		i;
-	int		*ptrcount;
+	int		*ptr_count;
 
 	count = 0;
-	ptrcount = &count;
+	ptr_count = &count;
 	va_start(args, format);
-	if (!format)
-		return (0);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			i = validate_format_specifier(*format);
-			if (i != 0)
-				printvalue(i, args, ptrcount);
-		}
-		else
-			count_putchar(*format, ptrcount);
-		format++;
-	}
+	input_handler(args, format, ptr_count);
+	va_end(args);
 	return (count);
-}
-
-static	int	validate_format_specifier(char *str)
-{
-	if (!ft_strncmp("%c", *str, 2))
-		return (1);
-	else if (!ft_strncmp("%s", *str, 2))
-		return (2);
-	else if (!ft_strncmp("%p", *str, 2))
-		return (3);
-	else if (!ft_strncmp("%d", *str, 2))
-		return (4);
-	else if (!ft_strncmp("%i", *str, 2))
-		return (4);
-	else if (!ft_strncmp("%u", *str, 2))
-		return (5);
-	else if (!ft_strncmp("%x", *str, 2))
-		return (6);
-	else if (!ft_strncmp("%X", *str, 2))
-		return (7);
-	else if (!ft_strncmp("%%", *str, 2))
-		return (1);
-	else
-		return (0);
-}
-
-static	void	printvalue(int specifier, va_list args, int *ptrcount)
-{
-	if (specifier == 1)
-		count_putchar(va_arg(args, char), ptrcount);
-	else if (specifier == 2)
-		count_putstr(va_arg(args, char *), ptrcount);
-	else if (specifier == 3)
-		count_putptr(va_arg(args, void *), ptrcount);
-	else if (specifier == 4)
-		count_putnbr(va_arg(args, int), ptrcount);
-	else if (specifier == 5)
-		count_putunsnbr(va_arg(args, unsigned int), ptrcount);
-	else if (specifier == 6)
-		count_puthex(va_arg(args, int), 0, ptrcount);
-	else if (specifier == 7)
-		count_puthex(va_arg(args, int), 1, ptrcount);
 }
