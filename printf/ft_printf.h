faae0adb55b65abf4f151b9d012b0ee15d563f95
Giovanni Bernardes Figueiredo <gaguiar-@student.42sp.org.br>
2025-08-15 01:52:44 -0300

    header rename

---
+++ printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 01:52:12 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/08/15 01:52:14 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+
+void	input_handler(va_list args, const char *format, int *count);
+char	*itohex(unsigned long ptr, char *base);
+int		ft_printf(const char *format, ...);
+void	print_char(char c, int *count);
+void	print_str(char *str, int *count);
+char	*ptr_to_str(void *ptr);
+void	put_count_nbr(int n, int *count);
+void	put_count_unbr(unsigned int n, int *count);
+
+#endif
